a228783f4b879656b9c883f0a9aa031eb63ec71d
Annise Arif <83833939+annise-arif@users.noreply.github.com>
2022-05-06 23:15:38 +0600

    updated footer

--- src/pages/share/Footer/Footer.js
+++ src/pages/share/Footer/Footer.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     const today = new Date();
     const currentyear = today.getFullYear();
     return (
-        <footer className='text-center mt-5 bg-black p-5'>
-            <p className='text-light'>Copy Right &copy; MD: Arif hasan {currentyear}</p>
+        <footer className='mt-5'>
+            <div className="row text-white">
+                <div className="col-lg-6 text-center text-lg-left mb-3 mb-lg-0">
+                    <p className="small mb-0 mt-1 ">Copy Right &copy; MD: Arif hasan {currentyear}</p>
+                </div>
+                <div className="col-lg-6 text-center text-lg-right">
+                    <a href="/">Home</a>
+                    <a href="/#"><i id='facebook' className="fab fa-facebook text-light"></i></a>
+                    <a href="/#"><i className="fab fa-youtube"></i></a>
+                    <a href="/#"><i className="fab fa-instagram"></i></a>
+                    <a href="/#"><i className="fab fa-twitter"></i></a>
+                </div>
+            </div>
         </footer>
     );
 };
